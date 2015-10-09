a871b207b9d060c60b330d29acd49029dabe81f2
Alex Piqueras <alomsimoy@gmail.com>
2015-10-09 16:44:12 +0000

    More optimization in isPrime function

--- IAW/Exercises/primeNumber.php
+++ IAW/Exercises/primeNumber.php
@@ -18,7 +18,8 @@ echo "The number ".$number." is".$result." prime.";
 
 // Check if a number is prime (boolean)
 function isPrime ($n) {
-    for ($i = 2; $i <= $n/2; $i++) {
+    if ($n % 2 == 0) return false; //Check if is divisible by 2
+    for ($i = 3; $i <= $n/2; $i += 2) { //Check only odd numbers
         if ($n % $i == 0) return false;
     }
     return true;
