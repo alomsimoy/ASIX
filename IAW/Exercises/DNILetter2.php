37d4753a95b97365bb368707510312a565f52cf1
alomsimoy <alomsimoy@gmail.com>
2015-10-26 18:33:05 +0100

    Corrections

--- IAW/Exercises/DNILetter2.php
+++ IAW/Exercises/DNILetter2.php
@@ -7,30 +7,54 @@
     <body>
 <?php
 
-$DNI=$PUSH[number];
-$letter=$PUSH[letter];
+$DNI=$_POST[number];
+$letter=$_POST[letter];
 
-echo "The letter of the DNI ".$DNI." is ".getDNILetter($DNI);
+echo $DNI;
+echo $letter;
+// checkDNI($DNI, $letter);
 
-function getHTML() {
-    switch (checkDNI) {
-        case 1:
-            //TODO
-    }
-}
 // checkDNI returns 1 if correct, 0 if not, -1 if number is not a number
 // and/or contain any char that is not a num, and -2 if letter is not a
 // single letter
 function checkDNI($number, $letter) {
-    if (!preg_match(\d{8}, $number)) {
-        return -1;
-    } else if (!preg_match([a-zA-Z], $letter)) {
-        return -2;
-    } else if (getDNILetter($number)==$letter) { 
-        return 1;
-    } else return 0;
+    $error=false;
+    echo $number.$letter;
+    if (!preg_match(\d(8), $number)) {
+        printMessage(2);
+        $error=true;
+    } 
+    if (!preg_match([a-zA-Z], $letter)) {
+        printMessage(3);
+        $error=true;
+    }
+    if (!$error) {
+        if (getDNILetter($number)==$letter) { 
+            printMessage(1);
+        } else {
+            printMessage(0);
+        }
+    }
 }
 
+function printMessage ($num) {
+    switch ($num) {
+        case 2:
+            echo "You must insert a valid DNI number (8 digits).<br/>";
+            break;
+        case 3:
+            echo "You must use a valid character between A and Z.<br/>";
+            break;
+        case 1:
+            echo "The letter match with the DNI number.<br/>";
+            break;
+        case 0:
+            echo "The letter does not match with the DNI number.<br/>";
+            break;
+        default:
+            echo "Unknown error.";
+    }
+}
 function getDNILetter ($DNI) {
     $num = $DNI % 23;
     $digit = explode(",","T,R,W,A,G,M,Y,F,P,D,X,B,N,J,Z,S,Q,V,H,L,C,K,E");
